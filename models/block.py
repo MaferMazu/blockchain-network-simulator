487925dc4e1656ab7db1333080903ef281570ba5
María Fernanda Magallanes Z <maria.magallanes@edunext.co>
2021-11-15 09:26:47 -0400

    docs: add more structure

---
+++ models/block.py
@@ -0,0 +1,14 @@
+"""Class Block"""
+
+class Block:
+    """Docs."""
+
+    def __init__(self):
+        # El bloque debería tener una lista de transacciones
+        # el hash
+        # las confirmaciones
+        # el peso
+        # el nodo que lo minó
+        # el merkle root con esta lib: https://github.com/Tierion/pymerkletools
+        pass
+
